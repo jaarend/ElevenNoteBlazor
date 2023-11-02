9fb737fb0ee5cd0a615c1fdddf1b19fe7fb5569a
Jayson Arend <jayson.arend@gmail.com>
2023-11-02 13:39:03 -0400

    created the cateory controller

---
+++ Server/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElevenNoteWebApp_2.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}

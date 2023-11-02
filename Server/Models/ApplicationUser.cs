60c8e09c0a0015d5f746961dad73904dd2077892
Jayson Arend <jayson.arend@gmail.com>
2023-11-02 11:11:50 -0400

    created note and category models

---
+++ Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ElevenNoteWebApp_2.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ Shared/Models/Note/NoteEdit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNoteWebApp_2.Shared.Models.Note
+{
+    internal class NoteEdit
+    {
+    }
+}

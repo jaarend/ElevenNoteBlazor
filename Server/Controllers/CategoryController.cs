e0eedfe9b0355895e4e687592a9f3593e513783c
Jayson Arend <jayson.arend@gmail.com>
2023-11-02 13:45:55 -0400

    modified navmenu

--- Server/Controllers/CategoryController.cs
+++ Server/Controllers/CategoryController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ElevenNoteWebApp_2.Server.Services.Category;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElevenNoteWebApp_2.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService; 
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Category(int id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+
+            if (category == null) return NotFound();
+            return Ok(category);
+        }
+
+
+
     }
 }
--- Server/Controllers/NoteController.cs
+++ Server/Controllers/NoteController.cs
@@ -2,6 +2,7 @@
 using ElevenNoteWebApp_2.Shared.Models.Note;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 using System.Security.Claims;
 
 namespace ElevenNoteWebApp_2.Server.Controllers
@@ -54,6 +55,51 @@ namespace ElevenNoteWebApp_2.Server.Controllers
             if(note == null) return NotFound();
 
             return Ok(note);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NoteCreate model)
+        {
+            if(model == null) return BadRequest();
+
+            if (!SetUserIdInService()) return Unauthorized();
+
+            bool wasSuccessful = await _noteService.CreateNoteAsync(model);
+
+            if (wasSuccessful) return Ok();
+            else return UnprocessableEntity();
+        }
+
+        [HttpPut("edit/{id}")]
+        public async Task<IActionResult> Edit(int id, NoteEdit model)
+        {
+            if(!SetUserIdInService()) return Unauthorized();
+
+            if(model == null || !ModelState.IsValid) return BadRequest();
+
+            if(model.Id != id) return BadRequest();
+
+            bool wasSuccessful = await _noteService.UpdateNoteAsync(model);
+
+            if(wasSuccessful) return Ok();
+            return BadRequest();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (!SetUserIdInService() return Unauthorized();
+
+            var note = await _noteService.GetNoteByIdAsync(id);
+
+            if(note == null) return NotFound();
+
+            bool wasSuccessful = await _noteService.DeleteNoteAsync(id);
+
+            if (!wasSuccessful) return BadRequest();
+
+            return Ok();
         }
 
     }
